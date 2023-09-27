3eff8eaa013ce90d6423f004933ba04583bfba7d
Ünver Can Ünlü <unvercanunlu@gmail.com>
2023-09-27 07:03:41 +0200

    api config is added.

---
+++ src/main/java/tr/unvercanunlu/compare_match/config/ApiConfig.java
@@ -0,0 +1,11 @@
+package tr.unvercanunlu.compare_match.config;
+
+public class ApiConfig {
+
+    public static final String VERSION = "v1";
+
+    public static final String BASE = "/" + "api" + "/" + VERSION;
+
+    public static final String SAMPLE_API = BASE + "/" + "sample";
+
+}

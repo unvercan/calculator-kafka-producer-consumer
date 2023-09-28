2f1a97ae1d7b7abb8571a8b2a48f2f535a531a7c
Ünver Can Ünlü <unvercanunlu@gmail.com>
2023-09-28 08:24:34 +0200

    to string method is added

--- src/main/java/tr/unvercanunlu/sample/model/entity/Sample.java
+++ src/main/java/tr/unvercanunlu/sample/model/entity/Sample.java
@@ -6,6 +6,7 @@ import lombok.*;
 import java.io.Serializable;
 import java.util.UUID;
 
+@ToString
 @Getter
 @Setter
 @Builder
--- src/main/java/tr/unvercanunlu/sample/model/entity/Sum.java
+++ src/main/java/tr/unvercanunlu/sample/model/entity/Sum.java
@@ -7,6 +7,7 @@ import org.springframework.data.mongodb.core.mapping.Document;
 import java.io.Serializable;
 import java.util.UUID;
 
+@ToString
 @Getter
 @Setter
 @Builder

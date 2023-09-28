aa02f9bf2f954e46a0c1d165eef3e23c30bf0aa2
Ünver Can Ünlü <unvercanunlu@gmail.com>
2023-09-28 10:02:06 +0200

    randomize method is added into sample service

--- src/main/java/tr/unvercanunlu/sample/service/ISampleService.java
+++ src/main/java/tr/unvercanunlu/sample/service/ISampleService.java
@@ -1,8 +1,10 @@
 package tr.unvercanunlu.sample.service;
 
-import jakarta.validation.constraints.NotNull;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.*;
 import org.springframework.validation.annotation.Validated;
 import tr.unvercanunlu.sample.model.entity.Sample;
+import tr.unvercanunlu.sample.model.request.SampleRequest;
 
 import java.util.List;
 import java.util.UUID;
@@ -16,6 +18,16 @@ public interface ISampleService {
 
     void delete(@NotNull(message = "Id should not be null.") UUID id);
 
-    Sample add(@NotNull(message = "Sample should not be null.") Sample sample);
+    Sample add(@NotNull(message = "Sample should not be null.") @Valid SampleRequest request);
+
+    List<Sample> randomize(
+            @Positive(message = "Count should be positive integer.")
+            @NotNull(message = "Count should not be null.")
+            @NotBlank(message = "Count should not be empty.")
+            @Min(value = 1, message = "Count should be at least one.")
+            @Max(value = 1000, message = "Count should be at max one thousand.")
+            @Digits(integer = 4, fraction = 0, message = "Count should be integer and at max one thousand.")
+            Integer count
+    );
 
 }
--- src/main/java/tr/unvercanunlu/sample/service/impl/SampleService.java
+++ src/main/java/tr/unvercanunlu/sample/service/impl/SampleService.java
@@ -5,18 +5,27 @@ import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.HttpClientErrorException;
 import tr.unvercanunlu.sample.model.entity.Sample;
+import tr.unvercanunlu.sample.model.request.SampleRequest;
 import tr.unvercanunlu.sample.repository.ISampleRepository;
+import tr.unvercanunlu.sample.service.IRandomService;
 import tr.unvercanunlu.sample.service.ISampleService;
 
 import java.util.List;
 import java.util.Objects;
 import java.util.UUID;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import java.util.stream.IntStream;
 
 @Service
 @RequiredArgsConstructor
 public class SampleService implements ISampleService {
 
+    private final Logger logger = Logger.getLogger(this.getClass().getName());
+
     private final ISampleRepository sampleRepository;
+
+    private final IRandomService randomService;
 
     @Override
     public List<Sample> getAll() {
@@ -38,6 +47,36 @@ public class SampleService implements ISampleService {
                 .orElseThrow(() -> new HttpClientErrorException(HttpStatus.NOT_FOUND));
 
         this.sampleRepository.deleteById(id);
+    }
+
+    @Override
+    public Sample add(SampleRequest request) {
+        Sample sample = Sample.builder()
+                .first(request.getFirst())
+                .second(request.getSecond())
+                .build();
+
+        sample = this.sampleRepository.save(sample);
+
+        this.logger.log(Level.INFO, String.format("Sample is created. Sample: %s", sample));
+
+        return this.sampleRepository.save(sample);
+    }
+
+    @Override
+    public List<Sample> randomize(Integer count) {
+        List<Sample> sampleList = IntStream.range(0, count)
+                .mapToObj(i -> Sample.builder()
+                        .first(this.randomService.generate(1, 10))
+                        .second(this.randomService.generate(1, 10))
+                        .build())
+                .toList();
+
+        sampleList = sampleList.stream().map(this.sampleRepository::save).toList();
+
+        sampleList.forEach(sample -> this.logger.log(Level.INFO, String.format("Sample is created. Sample: %s", sample)));
+
+        return sampleList;
     }
 
 }

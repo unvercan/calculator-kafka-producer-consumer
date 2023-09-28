6fcf64c16bdb03ab4164161b71d7c69ddadda055
Ünver Can Ünlü <unvercanunlu@gmail.com>
2023-09-28 08:36:37 +0200

    validations are added to sample service.

--- src/main/java/tr/unvercanunlu/sample/service/ISampleService.java
+++ src/main/java/tr/unvercanunlu/sample/service/ISampleService.java
@@ -1,16 +1,21 @@
 package tr.unvercanunlu.sample.service;
 
+import jakarta.validation.constraints.NotNull;
+import org.springframework.validation.annotation.Validated;
 import tr.unvercanunlu.sample.model.entity.Sample;
 
 import java.util.List;
 import java.util.UUID;
 
+@Validated
 public interface ISampleService {
 
     List<Sample> getAll();
 
-    Sample get(UUID id);
+    Sample get(@NotNull(message = "Id should not be null.") UUID id);
 
-    void delete(UUID id);
+    void delete(@NotNull(message = "Id should not be null.") UUID id);
+
+    Sample add(@NotNull(message = "Sample should not be null.") Sample sample);
 
 }

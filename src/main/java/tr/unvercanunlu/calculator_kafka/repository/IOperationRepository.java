aab01d73a1dbf195ac504eda2ba7feff83cf7771
Ünver Can Ünlü <unvercanunlu@gmail.com>
2023-10-03 22:58:17 +0200

    repositories are updated.

--- src/main/java/tr/unvercanunlu/calculator_kafka/repository/IOperationRepository.java
+++ src/main/java/tr/unvercanunlu/calculator_kafka/repository/IOperationRepository.java
@@ -2,11 +2,11 @@ package tr.unvercanunlu.calculator_kafka.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-import tr.unvercanunlu.calculator_kafka.model.entity.Result;
+import tr.unvercanunlu.calculator_kafka.model.entity.Operation;
 
 import java.util.UUID;
 
 @Repository
-public interface IResultRepository extends JpaRepository<Result, UUID> {
+public interface IOperationRepository extends JpaRepository<Operation, UUID> {
 
 }
